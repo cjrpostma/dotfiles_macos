46116baa87067e785e164e7bf0e8a5980600e787
Chris Postma <44818815+cjrpostma@users.noreply.github.com>
2025-02-16 12:11:50 -0500

    nvim updates

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,13 +3,19 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = {
-      style = "moon",
+      style = "storm",
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
     },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
   },
   {
     "LazyVim/LazyVim",
